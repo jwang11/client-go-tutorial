88fe848f775cd6c68c90dc3f8e0cd8ebfed6c0a4
jwang <13162811980@qq.com>
2022-01-24 06:03:03 +0000

    add discover client in list sample

--- list/list.go
+++ list/list.go
@@ -13,6 +13,7 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/tools/clientcmd"
         "k8s.io/client-go/util/homedir"
 
@@ -113,6 +114,37 @@ func DynamicClient(kubeconfig *string) {
 	}
 }
 
+func DiscoverClient(kubeconfig *string) {
+	config, _ := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+
+	discoveryClient, _ := discovery.NewDiscoveryClientForConfig(config)
+
+	fmt.Printf("\n--------------DynamicClient-----------\n")
+	// 获取所有分组和资源数据
+	APIGroup, APIResourceListSlice, _ := discoveryClient.ServerGroupsAndResources()
+	// 先看Group信息
+	fmt.Printf("APIGroup :\n %v\n\n",APIGroup)
+
+	// APIResourceListSlice是个切片，里面的每个元素代表一个GroupVersion及其资源
+	for _, singleAPIResourceList := range APIResourceListSlice {
+
+		// GroupVersion是个字符串，例如"apps/v1"
+		groupVerionStr := singleAPIResourceList.GroupVersion
+
+		// ParseGroupVersion方法将字符串转成数据结构
+		gv, _ := schema.ParseGroupVersion(groupVerionStr)
+
+		fmt.Printf("\n*********************%v********************************\n", groupVerionStr)
+		fmt.Printf("GV struct [%#v]\n", gv)
+
+		fmt.Printf("resources:\n")
+		// APIResources字段是个切片，里面是当前GroupVersion下的所有资源
+		for _, singleAPIResource := range singleAPIResourceList.APIResources {
+			fmt.Printf("\t%v\n", singleAPIResource.Name)
+		}
+	}
+}
+
 
 func main() {
         var kubeconfig *string
@@ -124,4 +156,5 @@ func main() {
 	RestClient(kubeconfig)
 	ClientSet(kubeconfig)
 	DynamicClient(kubeconfig)
+	DiscoverClient(kubeconfig)
 }
